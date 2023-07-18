5dd8e731957fe2b92932a530ce48ab984db12455
Daniel Castillo <Daniel.302009@hotmail.com>
2023-07-17 21:08:07 -0500

    routes users

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -3,7 +3,19 @@ const {Router} = require('express');
 const usersRouter = Router();
 
 usersRouter.get('/', (req, res) => {
-    res.status(200).send('estoy en usuarios')
-})
+    res.status(200).send('estoy en usuarios');
+});
+
+usersRouter.post('/', (req, res) => {
+    res.status(201).send('creando un usuario');
+});
+
+usersRouter.post('/login', (req, res) => {
+    res.status(201).send('login');
+});
+
+usersRouter.put('/', (req, res) => {
+    res.status(200).send('actualizando datos de un usuario');
+});
 
 module.exports = { usersRouter }
